e0333a18a3c834a8973cbabb173e517890d5d396
ahmedsobh036 <ahmedsobh036@gmail.com>
2025-02-11 12:05:52 -0500

    create mock home page and add default profile picture image

---
+++ app/home/page.tsx
@@ -0,0 +1,29 @@
+import { auth } from "@/auth";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+
+const HomePage = async () => {
+    const session = await auth();
+
+
+    if(!session?.user) redirect("/");
+
+
+    const userImage = session?.user?.image ?? "/path/to/default/image.png"; // Provide a default image
+
+    return(
+        <div className="flex flex-col items-center m-4">
+            <h1>{session?.user?.name}</h1>
+
+            <Image
+                src={userImage}
+                alt={session?.user?.name || "User Image"}
+                width={72}
+                height={72}
+                className="rounded-full" />
+        </div>
+    )
+}
+
+export default HomePage
+
