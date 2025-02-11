365f9447f5340bc2b7b20a8dcbf8532b1a4dc6b4
ahmedsobh036 <ahmedsobh036@gmail.com>
2025-02-11 11:01:47 -0500

    create action function for login

---
+++ app/actions/index.ts
@@ -0,0 +1,10 @@
+'use server'
+
+export async function doSocialLogin(formData: FormData){
+    const action = formData.get('action');
+    console.log(action);
+}
+
+export async function doLogout(){
+
+}
--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,6 +1,8 @@
+import { doSocialLogin } from "@/app/actions"
+
 const LoginForm = () => {
     return (
-        <form>
+        <form action={doSocialLogin}>
             <button
                 className="bg-black text-white p-1 rounded-md m-1 text-1g"
                 type="submit" name="action" value="google">
