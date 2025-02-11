886b292828ab34b0382145c3c108d0e999ccd204
ahmedsobh036 <ahmedsobh036@gmail.com>
2025-02-11 10:50:01 -0500

    create LoginForm component

---
+++ components/LoginForm.tsx
@@ -0,0 +1,13 @@
+const LoginForm = () => {
+    return (
+        <form>
+            <button
+                className="bg-black text-white p-1 rounded-md m-1 text-1g"
+                type="submit" name="action" value="google">
+                Sign In With Google
+            </button>
+        </form>
+    )
+}
+
+export default LoginForm
