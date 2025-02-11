35f42fbc38902cc591ad6f33ba028bccb3763927
ahmedsobh036 <ahmedsobh036@gmail.com>
2025-02-11 11:50:25 -0500

    call signIn method and redirect to home page when signed in

--- app/actions/index.ts
+++ app/actions/index.ts
@@ -1,9 +1,17 @@
 'use server'
 
-export async function doSocialLogin(formData: FormData){
+import { signIn } from "@/auth"
+
+export async function doSocialLogin(formData: FormData) {
     const action = formData.get('action');
-    console.log(action);
+    if (typeof action === 'string') {
+        await signIn(action, { redirectTo: "/home" });
+        console.log(action);
+    } else {
+        console.error("Invalid action value");
+    }
 }
+
 
 export async function doLogout(){
 
