a7f740697e5d2eace3ac78d2c3892f27ccbf26b5
abdullah_badat18 <abdullahbadat@hotmail.com>
2025-02-14 22:59:52 -0500

    Fix the npm start to work

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,10 +3,10 @@ export default function Sidebar() {
       <aside className="bg-white shadow-md p-4 rounded-md flex flex-col space-y-4">
         <input
           type="text"
-          placeholder="Search"
+          placeholder="Search Users"
           className="w-full p-2 border rounded-md"
         />
-        <button className="bg-gray-300 px-4 py-2 rounded-md">Games</button>
+        <button className="bg-gray-300 px-4 py-2 rounded-md">Search</button>
       </aside>
     );
   }
