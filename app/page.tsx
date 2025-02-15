58f4a04097722c002617b165d4061d4ff8041ecb
abdullah_badat18 <abdullahbadat@hotmail.com>
2025-02-14 22:00:46 -0500

    Add posts on root page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
+import Post from "@/components/Post";
+
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col items-center gap-6 p-6 w-full">
+      <section className="flex flex-col space-y-6 max-w-2xl w-full">
+        <Post />
+        <Post />
+        <Post />
+      </section>
     </div>
   );
 }
