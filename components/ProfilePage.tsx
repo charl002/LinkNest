12d7b885c9f4f653b3af3e444625694122663a75
MohammadRahman <rahmanmahbub0529@gmail.com>
2025-02-20 12:15:06 -0500

    Add api to get user by username

--- components/ProfilePage.tsx
+++ components/ProfilePage.tsx
@@ -1,4 +1,8 @@
+"use client";
+
 export default function ProfilePage( { user }: { user: string } ) {
+
+  
     return (
       <div className="p-6">
         <h1 className="text-2xl font-bold">{user} Profile</h1>
