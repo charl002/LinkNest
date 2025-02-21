6186e0748832baae3eb21b1c92f9a090bb673e3c
abdullah_badat18 <abdullahbadat@hotmail.com>
2025-02-21 00:34:25 -0500

    Implement a createpost route

---
+++ app/createpost/page.tsx
@@ -0,0 +1,9 @@
+import CreatePost from "@/components/CreatePost";
+
+export default function CreatePostPage() {
+  return (
+    <div className="p-6">
+      <CreatePost />
+    </div>
+  );
+}
--- components/CreatePost.tsx
+++ components/CreatePost.tsx
@@ -1,4 +1,5 @@
-"use-client";
+"use client";
+
 import { useState, useRef } from "react";
 // import Image from "next/image";
 import { useSession } from "next-auth/react"; 
