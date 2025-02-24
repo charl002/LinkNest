cae653d84df66a462c6021ab9c3276c242888b6a
PepeLaugh <charles-alexandre.bouchard@dawsoncollege.qc.ca>
2025-02-23 19:15:34 -0500

    change hostname

--- server.ts
+++ server.ts
@@ -4,7 +4,7 @@ import next from "next";
 import { Server as SocketIOServer } from "socket.io";
 
 const dev = process.env.NODE_ENV !== "production";
-const hostname = "0.0.0.0";
+const hostname = process.env.WEBSITE_HOSTNAME || "localhost";
 const port = Number(process.env.PORT) || 3000;
 const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
