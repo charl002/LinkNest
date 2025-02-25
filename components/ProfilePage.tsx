546fb68fe64906209d6ff957ba2596399e355b99
MohammadRahman <rahmanmahbub0529@gmail.com>
2025-02-24 23:26:09 -0500

    Create add friend button

--- components/ProfilePage.tsx
+++ components/ProfilePage.tsx
@@ -129,9 +129,13 @@ export default function ProfilePage({ user }: { user: string }) {
                 <br />
                 <p className="text-gray-700">{userData.data.description}</p>
               </div>
-              {userData.data.email === email && (
+              {userData.data.email === email ? (
                 <button className="px-4 py-2 bg-blue-500 text-white text-sm rounded-full">
                   Profile settings
+                </button>
+              ) : (
+                <button className="px-4 py-2 bg-blue-500 text-white text-sm rounded-full">
+                  Add Friend
                 </button>
               )}
             </div>
