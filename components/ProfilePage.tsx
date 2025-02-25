e7d16404ce112cafadc789e6051642e00bd1d124
MohammadRahman <rahmanmahbub0529@gmail.com>
2025-02-25 10:57:53 -0500

    Add a form for editing profile

--- components/ProfilePage.tsx
+++ components/ProfilePage.tsx
@@ -4,6 +4,18 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 import Post from "./Post"; 
 import { useSession } from "next-auth/react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Button } from "@/components/ui/button"
 
 interface UserData {
   id: string;
@@ -130,9 +142,38 @@ export default function ProfilePage({ user }: { user: string }) {
                 <p className="text-gray-700">{userData.data.description}</p>
               </div>
               {userData.data.email === email ? (
-                <button className="px-4 py-2 bg-blue-500 text-white text-sm rounded-full">
-                  Profile settings
-                </button>
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <button className="px-4 py-2 bg-blue-500 text-white text-sm rounded-full">
+                      Profile settings
+                    </button>
+                  </DialogTrigger>
+                  <DialogContent className="sm:max-w-[425px]">
+                    <DialogHeader>
+                      <DialogTitle>Edit profile</DialogTitle>
+                      <DialogDescription>
+                        Make changes to your profile here. Click save when you are done.
+                      </DialogDescription>
+                    </DialogHeader>
+                    <div className="grid gap-4 py-4">
+                      <div className="grid grid-cols-4 items-center gap-4">
+                        <Label htmlFor="name" className="text-right">
+                          Username
+                        </Label>
+                        <Input id="username" value={userData.data.username} className="col-span-3" />
+                      </div>
+                      <div className="grid grid-cols-4 items-center gap-4">
+                        <Label htmlFor="username" className="text-right">
+                          Description
+                        </Label>
+                        <Input id="description" value={userData.data.description} className="col-span-3" />
+                      </div>
+                    </div>
+                    <DialogFooter>
+                      <Button type="submit">Save changes</Button>
+                    </DialogFooter>
+                  </DialogContent>
+                </Dialog>
               ) : (
                 <button className="px-4 py-2 bg-blue-500 text-white text-sm rounded-full">
                   Add Friend
