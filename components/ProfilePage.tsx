39e9b938a9d9ad88f60af191d698fb17777d59e6
MohammadRahman <rahmanmahbub0529@gmail.com>
2025-02-24 23:07:44 -0500

    Add scrolling to post section of profile

--- components/ProfilePage.tsx
+++ components/ProfilePage.tsx
@@ -148,7 +148,7 @@ export default function ProfilePage({ user }: { user: string }) {
             </div>
           </div>
 
-          <div className="p-4 space-y-6">
+          <div className="p-4 space-y-6 overflow-y-auto max-h-[calc(100vh-320px)]">
             {postsCount > 0 ? (posts.map((post, index) => 
               <Post 
                   key={`${post.id}-${index}`} 
