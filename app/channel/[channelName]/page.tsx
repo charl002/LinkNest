323fe18f204d0a9faf439d2b1e34fadd94c2b737
MohammadRahman <rahmanmahbub0529@gmail.com>
2025-03-04 12:45:22 -0500

    Fix build error

--- app/channel/[channelName]/page.tsx
+++ app/channel/[channelName]/page.tsx
@@ -1,14 +1,21 @@
+import { Toaster } from "sonner";
 import Call from "@/components/chat/Call";
 
-export default function Page({ params }: { params: { channelName: string } }) {
-    const channelName = params.channelName || "root";
+type CallParams = Promise<{ channelName: string }>;
+
+const Page = async (props: { params: CallParams }) => {
+    const { channelName } = await props.params;
+    const channel = channelName || "root";
 
     return (
         <main className="flex w-full flex-col">
             <p className="absolute z-10 mt-2 ml-12 text-2xl font-bold text-gray-900">
-                {channelName}
+                {channel}
             </p>
-            <Call appId={process.env.NEXT_PUBLIC_AGORA_APP_ID!} channelName={channelName} />
+            <Call appId={process.env.NEXT_PUBLIC_AGORA_APP_ID!} channelName={channel} />
+            <Toaster position="bottom-center" richColors />
         </main>
     );
-}
+};
+
+export default Page;
