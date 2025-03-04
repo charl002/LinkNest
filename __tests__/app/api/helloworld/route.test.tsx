e23d4b2ac2cc923ed5c44d9b5e7e700e1136437e
mintthiha <mintthiha@gmail.com>
2025-03-04 13:32:56 -0500

    Fix auth on prod, where error says you are not allowed to see ms

---
+++ __tests__/app/api/helloworld/route.test.tsx
@@ -0,0 +1,13 @@
+// import { GET } from '../../../../app/api/helloworld/route';
+// import { NextResponse } from 'next/server';
+
+// describe("/api/helloworld API Endpoint", () => {
+//   test("returns a 200 status and correct response", async () => {
+//     const response = await GET();
+
+//     const data = await response.json();
+
+//     expect(response).toBeInstanceOf(NextResponse);
+//     expect(data).toEqual({ message: "Hello from API Route!" });
+//   });
+// });
