db537f72e82f589929e9130ac5057cd40210e5c4
abdullah_badat18 <abdullahbadat@hotmail.com>
2025-03-18 18:38:14 -0400

    Minor Fix

--- lib/cache.ts
+++ lib/cache.ts
@@ -1,4 +1,4 @@
 import NodeCache from 'node-cache';
 
-const cache = new NodeCache({ stdTTL: 60 }); // 10mins cache
+const cache = new NodeCache({ stdTTL: 600 }); // 10mins cache
 export default cache;
