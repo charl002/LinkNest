b1d2a331bc0762c045a12536c02bc115074e8f76
mintthiha <mintthiha@gmail.com>
2025-03-17 20:32:21 -0400

    Fix CSS for Create Post Page

--- app/createpost/page.tsx
+++ app/createpost/page.tsx
@@ -1,17 +1,17 @@
-import ChatList from "@/components/chat/ChatList";
-import CreatePost from "@/components/post/CreatePost";
-import Sidebar from "@/components/custom-ui/Sidebar";
-import { Toaster } from "sonner";
+import ChatList from "@/components/chat/ChatList"
+import CreatePost from "@/components/post/CreatePost"
+import Sidebar from "@/components/custom-ui/Sidebar"
+import { Toaster } from "sonner"
 
 export default function CreatePostPage() {
-    return (
-        <div className="grid grid-cols-[300px_2fr_300px] gap-6 p-6 w-full min-h-screen bg-gray-100">
-            <Sidebar />
-            <div className="bg-white shadow-md rounded-lg p-8 flex items-center justify-center">
-                <CreatePost />
-            </div>
-            <ChatList />
-            <Toaster position="bottom-center" richColors></Toaster>
-        </div>
-    );
+  return (
+    <div className="grid grid-cols-[300px_2fr_300px] gap-6 p-6 w-full bg-gray-100">
+      <Sidebar />
+      <div className="bg-white shadow-md rounded-lg p-8 flex items-center justify-center flex-grow overflow-auto h-[calc(100vh-120px)]">
+        <CreatePost />
+      </div>
+      <ChatList />
+      <Toaster position="bottom-center" richColors></Toaster>
+    </div>
+  );
 }
