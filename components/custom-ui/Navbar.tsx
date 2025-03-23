91668120ffcf86c567e248fa9f14a681be0ffe3f
ahmedsobh036 <ahmedsobh036@gmail.com>
2025-03-23 12:06:41 -0400

    add nightmode button on navbar

--- components/custom-ui/Navbar.tsx
+++ components/custom-ui/Navbar.tsx
@@ -16,6 +16,36 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { House } from 'lucide-react';
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import { Button } from "@/components/ui/button";
+
+export function ModeToggle(){
+  const { setTheme } = useTheme();
+
+  return ( 
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+      <Button variant="outline" size="icon">
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
 
 const Navbar = () => {
 
@@ -57,6 +87,7 @@ const Navbar = () => {
         </h1>
         
         <div className="flex items-center space-x-3">
+          <ModeToggle/>
           {session?.user ? (
             // If user is logged in, show profile image, username & logout button
             <>
