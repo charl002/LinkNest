007c1db6f496cedfdd77a631835407c4bbc10ba0
MohammadRahman <rahmanmahbub0529@gmail.com>
2025-03-30 00:48:26 -0400

    Add health check for the website

---
+++ app/health/page.tsx
@@ -0,0 +1,9 @@
+import HealthCheck from "@/components/health/HealthCheck";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <HealthCheck />
+    </div>
+  );
+}
