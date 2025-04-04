93131e26475b19d594d3b02c298b89b1e886096a
mintthiha <mintthiha@gmail.com>
2025-04-03 23:43:24 -0400

    Reverting one last time

--- components/provider/SocketProvider.tsx
+++ components/provider/SocketProvider.tsx
@@ -3,16 +3,18 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { io, Socket } from "socket.io-client";
 
+// Will need to fix this logic later, this works for now on prod.
 const isDev = process.env.NODE_ENV !== "production";
 const socketUrl = isDev 
-  ? "ws://linknest.live" 
-  : "wss://linknest.live";
+  ? "ws://localhost:3000" 
+  : "wss://linknest-fkd5eba5dqbrhzd7.canadacentral-01.azurewebsites.net";
 
 const SocketContext = createContext<Socket | null>(null);
 
 export function SocketProvider({ children }: { children: React.ReactNode }) {
   const [socket, setSocket] = useState<Socket | null>(null);
 
+  // Create the Socket object.
   useEffect(() => {
     const socketInstance: Socket = io(socketUrl, {
       transports: ["websocket", "polling"],
@@ -28,14 +30,6 @@ export function SocketProvider({ children }: { children: React.ReactNode }) {
       console.log("Disconnected from WebSocket");
     });
 
-    // socketInstance.on("privateMessage", (data) => {
-    //   console.log("Received privateMessage:", data); // Log received message
-    // });
-
-    // socketInstance.on("groupMessage", (data) => {
-    //     console.log("Received group Message:", data); 
-    // });
-
     return () => {
       socketInstance.disconnect();
       console.log("Socket disconnected");
@@ -45,6 +39,7 @@ export function SocketProvider({ children }: { children: React.ReactNode }) {
   return <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>;
 }
 
+// Allow 1 instance of 'socket' to be used.
 export function useSocket() {
   return useContext(SocketContext);
 }
