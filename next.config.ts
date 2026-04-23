0db977812e39a5be90bce63fa4776d9fcad06e96
mintthiha <mintthiha@gmail.com>
2026-04-22 23:42:41 -0400

    Update outdated README and ts file

--- next.config.ts
+++ next.config.ts
@@ -12,7 +12,7 @@ const nextConfig: NextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200],
     imageSizes: [16, 32, 48, 64, 96, 128, 256],
     formats: ['image/webp'],
-    domains: ['cdn.bsky.app', 'res.cloudinary.com'],
+    domains: ['cdn.bsky.app', 'res.cloudinary.com', 'webprojazure.blob.core.windows.net'],
     unoptimized: process.env.NODE_ENV === 'development',
   },
   experimental: {
